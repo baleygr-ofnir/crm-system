92421b7af061f76344c321ff620fd82e37afe0ec
Jorma Lindh <baleygr.jarngrimr@proton.me>
2026-05-16 19:36:01 +0200

    fix: auto-init ID property with a string Guid

--- api/Data/Entities/Customer.cs
+++ api/Data/Entities/Customer.cs
@@ -4,7 +4,8 @@ namespace api.Data.Entities;
 
 public record Customer
 {
-    [JsonProperty("id")] public string Id { get; init; } = Guid.NewGuid().ToString();
+    [JsonProperty("id")]
+    public string Id { get; init; } = Guid.NewGuid().ToString();
 
     [JsonProperty("name")]
     public required string Name { get; init; }
