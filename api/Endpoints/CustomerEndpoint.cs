b866cc6cdb83cd544af01ed788173559ef30d451
Jorma Lindh <baleygr.jarngrimr@proton.me>
2026-05-16 19:02:38 +0200

    customer endpoints configured

--- api/Endpoints/CustomerEndpoint.cs
+++
@@ -1,6 +0,0 @@
-namespace api.Endpoints;
-
-public static class CustomerEndpoint
-{
-    public static IEndpointRouteBuilder MapCustomerEndpoint(this IEndpointRouteBuilder endpoints)
-}
---
+++ api/Endpoints/CustomerEndpoints.cs
@@ -0,0 +1,64 @@
+using api.Core.Services;
+using api.Data.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.Cosmos;
+
+namespace api.Endpoints;
+
+public static class CustomerEndpoints
+{
+    public static IEndpointRouteBuilder MapCustomerEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var group = endpoints.MapGroup("/api/customers");
+
+        // CREATE
+        group.MapPost("/", async (Customer newCustomer, CustomerService dbService) =>
+        {
+            var response = await dbService.CreateCustomerAsync(newCustomer);
+
+            return Results.Created($"/api/customers/{response.Id}", response);
+        });
+
+        // GET CUSTOMER
+        group.MapGet("/{id}", async (string id, CustomerService dbService) =>
+        {
+            var response = await dbService.GetCustomerAsync(id);
+
+            return (response is { } customer)
+                ? Results.Ok(customer)
+                : Results.NotFound();
+        });
+
+        // GET CUSTOMERS
+        group.MapGet("/", async (CustomerService dbService) =>
+        {
+            var response = await dbService.GetCustomersAsync();
+
+            return (response is { } customers)
+                ? Results.Ok(customers)
+                : Results.NotFound();
+        });
+
+        // UPDATE CUSTOMER
+        group.MapPut("/{id}", async (Customer updatedCustomer, string id, CustomerService dbService) =>
+        {
+            var response = await dbService.UpdateCustomerAsync(updatedCustomer, id);
+
+            return (response is { } updated)
+                ? Results.Ok(updated)
+                : Results.NotFound();
+        });
+
+        group.MapDelete("/{id}", async (string id, CustomerService dbService) =>
+        {
+            var response = await dbService.DeleteCustomerAsync(id);
+
+            return response
+                ? Results.NoContent()
+                : Results.NotFound();
+        });
+
+        return endpoints;
+    }
+}
